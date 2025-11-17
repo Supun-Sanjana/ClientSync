8de52d309a3ed017f73bc126353fd0951bf2c0ae
Supun Sanjana <infor.ssupun@gmail.com>
2025-11-17 21:27:37 +0530

    change port for railway

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import app from "./src/app.js";
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`server is running on port ${port}`);
 });
--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-import pool from "./config/db.js";
 import clientRouter from "./router/client.route.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
-pool.connect();
-
-// app.use("/", (_, res) => res.sendStatus(200));
 
 app.use("/api/client", clientRouter);
 
