3c437bd5fc0b635339e72f33982a7c89fa2317c7
Supun Sanjana <infor.ssupun@gmail.com>
2025-11-18 14:45:24 +0530

    setup cors

--- src/app.js
+++ src/app.js
@@ -2,19 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 import clientRouter from "./router/client.route.js";
 import projectRouter from "./router/project.route.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// inside app.js
+// Use CORS globally
+const corsOptions = {
+  origin: 'http://localhost:5173', // remove trailing slash
+  optionsSuccessStatus: 200 // legacy browsers
+};
+app.use(cors(corsOptions))
+
 app.get("/", (req, res) => {
   res.send("API is alive!");
 });
 
-
 app.use("/api/client", clientRouter);
 app.use("/api/project", projectRouter);
-
 
 export default app;
